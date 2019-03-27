20851d2f887bb702a3dfc66e03ee986f36fa22e3
JHMoreno <48996662+JHMoreno@users.noreply.github.com>
2019-03-27 15:01:01 -0600

    Add files via upload

---
+++ main/SchoolManager.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package schoolmanager.main;
+
+import schoolmanager.object.School;
+
+/**
+ *
+ * @author JMorenoM
+ */
+public class SchoolManager {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        School school = new School("Buena Tierra");
+        System.out.println(school.toString());
+    }
+}
